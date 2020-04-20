9bf7882b1fea7b0f3b67e7dfe48acee7f501d766
rickwilcoxen <rickwilcoxen95@gmail.com>
2020-04-20 09:37:55 -0400

    Created basic skeleton for the entire website
    Created pages: about.html, contact, projects, resume
    Created api, events, app, and ui.js for use just in case
    Created index.js
    Overall created the skeleton for everything

---
+++ assets/scripts/app.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const authEvents = require('./auth/events.js')
+
+$(() => {
+
+})
---
+++ assets/scripts/auth/events.js
@@ -0,0 +1,8 @@
+'use strict'
+// const getFormFields = require('../../../lib/get-form-fields.js')
+// const api = require('./api.js')
+// const ui = require('./ui.js')
+
+module.exports = {
+
+}
